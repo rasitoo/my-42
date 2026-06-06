5bfd7e5dabe2062b96bf63f60d66e7fcaa2bf31b
Rodrigo Tapiador Cano <rtapiado@c3r11s4.42madrid.com>
2026-06-06 13:46:46 +0200

    fixes

--- campus/milestone0/piscine_reloaded/ex12/ft_iterative_factorial.c
+++ campus/milestone0/piscine_reloaded/ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: rtapiado <rtapiado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 13:20:18 by rtapiado          #+#    #+#             */
-/*   Updated: 2026/06/06 13:26:34 by rtapiado         ###   ########.fr       */
+/*   Updated: 2026/06/06 13:44:24 by rtapiado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ int	ft_iterative_factorial(int nb)
 {
 	int	result;
 
+	if (nb < 0)
+		return (0);
 	result = nb;
 	while (nb > 1)
 		result *= --nb;
--- campus/milestone0/piscine_reloaded/ex13/ft_recursive_factorial.c
+++ campus/milestone0/piscine_reloaded/ex13/ft_recursive_factorial.c
@@ -6,12 +6,14 @@
 /*   By: rtapiado <rtapiado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 13:27:39 by rtapiado          #+#    #+#             */
-/*   Updated: 2026/06/06 13:33:14 by rtapiado         ###   ########.fr       */
+/*   Updated: 2026/06/06 13:44:39 by rtapiado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
+	if (nb < 0)
+		return (0);
 	if (nb == 1)
 		return (1);
 	else

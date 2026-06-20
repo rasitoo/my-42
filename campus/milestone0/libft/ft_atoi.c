ffd78914b0fb8034b82ce3dcac38b3bf84b1e73c
rasitoo <05rotapi@gmail.com>
2026-06-20 19:13:53 +0200

    atoi correction

--- campus/milestone0/libft/ft_atoi.c
+++ campus/milestone0/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rtapiado <rtapiado@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 16:31:29 by rtapiado          #+#    #+#             */
-/*   Updated: 2026/06/18 19:24:07 by rtapiado         ###   ########.fr       */
+/*   Updated: 2026/06/20 19:13:31 by rtapiado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	while ((nptr[i] >= 8 && nptr[i] <= 13) || nptr[i] == ' ')
 		i++;
-	while (nptr[i] == '-' || nptr[i] == '+')
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
 			neg = -neg;
@@ -37,37 +37,37 @@ int	ft_atoi(const char *nptr)
 	return (res * neg);
 }
 
-int	main(void)
-{
-	char	*tests[] = {
-		"42",
-		"-42",
-		"+42",
-		"    42",
-		"\t\n\v\f\r 42",
-		"0042",
-		"-0042",
-		"+++---++-+-+--++-++++--+-42abc",
-		"abc42",
-		"",
-		"+",
-		"-"
-	};
-	int	i;
+// int	main(void)
+// {
+// 	char	*tests[] = {
+// 		"42",
+// 		"-42",
+// 		"+42",
+// 		"    42",
+// 		"\t\n\v\f\r 42",
+// 		"0042",
+// 		"-0042",
+// 		"+++---++-+-+--++-++++--+-42abc",
+// 		"abc42",
+// 		"",
+// 		"+",
+// 		"-"
+// 	};
+// 	int	i;
 
-	i = 0;
-	while (i < 12)
-	{
-		printf("Entrada: \"%s\"\n", tests[i]);
-		printf("ft_atoi: %d\n", ft_atoi(tests[i]));
-		printf("atoi   : %d\n", atoi(tests[i]));
+// 	i = 0;
+// 	while (i < 12)
+// 	{
+// 		printf("Entrada: \"%s\"\n", tests[i]);
+// 		printf("ft_atoi: %d\n", ft_atoi(tests[i]));
+// 		printf("atoi   : %d\n", atoi(tests[i]));
 
-		if (ft_atoi(tests[i]) == atoi(tests[i]))
-			printf("OK\n\n");
-		else
-			printf("ERROR\n\n");
+// 		if (ft_atoi(tests[i]) == atoi(tests[i]))
+// 			printf("OK\n\n");
+// 		else
+// 			printf("ERROR\n\n");
 
-		i++;
-	}
-	return (0);
-}
+// 		i++;
+// 	}
+// 	return (0);
+// }
